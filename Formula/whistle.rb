bda465774daa679e73ebca554982c79505b5c117
jiewei.ljw <jiewei.ljw@alibaba-inc.com>
2019-12-29 20:51:43 +0800

    rm useless comment

--- Formula/whistle.rb
+++ Formula/whistle.rb
@@ -4,7 +4,6 @@ class Whistle < Formula
   desc "HTTP, HTTP2, HTTPS, Websocket debugging proxy"
   homepage "https://github.com/avwo/whistle"
   url "https://registry.npmjs.org/whistle/-/whistle-2.4.2.tgz"
-  # version "2.4.2"
   sha256 "bff72cdeba659c66a709c9910c7427ba9bc5ef25a39cc237288f5877ff9a4f62"
 
   bottle do
